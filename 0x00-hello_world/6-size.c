131b82e570d32e660ae0de604eb391f9b07d4b41
Constance Mhlabane <tessamhlabane31@gmail.com>
2022-12-08 10:47:03 -0800

    Size C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include e <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("size of a char: %i byte(s)\n", sizeof(char));
+	printf("size of a int: %i byte(s)\n", sizeof(int));
+	printf("size of a long: %i byte(s)\n", sizeof(long));
+	printf("size of a long long int: %i byte(s)\n", sizeof(long long int));
+	printf("size of a float: %i byte(s)\n", size(float));
+	return (0);
+}
