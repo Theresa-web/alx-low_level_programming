1549b25f3373da57d384a93fc5674a87db18985b
Constance Mhlabane <tessamhlabane31@gmail.com>
2022-12-09 10:20:16 -0800

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Sucess/correct)
+ */
+int main(void)
+{
+	char sam;
+
+	for (sam = 'a'; sam <= 'z'; samm++)
+	{
+		putchar(sam);
+	}
+	putchar("\n");
+	return (0);
+}
