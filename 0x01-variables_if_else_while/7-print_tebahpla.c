588e96b8a9064e053a65832209d1f62daf3b8615
Constance Mhlabane <tessamhlabane31@gmail.com>
2022-12-09 19:52:55 -0800

    Printting numbers and letters

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print the alpgabets in reversee
+ * Return: Always 0
+ */
+into main(void)
+{
+	char le;
+	
+	for (le = 'z'; le >= 'a'; le--)
+		putchar(le);
+	putchar('\n');
+	return (0);
+}
