4a9f2213a2bfe0393bb53400db6da8966667db64
Theresa-web <73585864+Theresa-web@users.noreply.github.com>
2022-12-12 20:36:04 +0200

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,32 +3,29 @@
 #include <time.h>
 
 /**
- *main - Entry point
- *
- *Return: Always 0 (Success)
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int a, b;
 
-	for (a = '0'; a < '9'; a++)
+	for (a = 0; a <= 98; a++)
 	{
-		for (b = a + 1; b <= '9'; b++)
+		for (b = a + 1; b <= 99; b++)
 		{
-			if (b != a)
-			{
-				putchar(a);
-				putchar(b);
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
 
-				if (a == '8' && b == '9')
-					continue;
-
-				putchar(',');
-				putchar(' ');
-			}
+			if (a == 98 && b == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
