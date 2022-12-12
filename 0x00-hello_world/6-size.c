998824ea2b8ec82cd78f115b3f56828f7426005b
Theresa-web <73585864+Theresa-web@users.noreply.github.com>
2022-12-12 22:19:03 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,17 @@
-#include <stdio.h>
+#include <stdio.h>                                                                                                                     
 /**
- * main - Receives no arguement value
- * Return: Always 0 (Success)
- */
+* main - Entry
+* Return: Always 0
+*/
+main(void)
 int main(void)
 {
-	int a;
-	long int b;
-	long long int c;
-	char d;
-	float e;
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a long int: %u byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long long int: %u bytes(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a char: %u bytes(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %u bytes(s)\n", (unsigned long)sizeof(e));
-	return (0);
+printf("Size of a char: %i byte(s) ", sizeof(char));
+printf("Size of an int: %i byte(s) ", sizeof(int));
+printf("Size of a long int: %i byte(s) ", sizeof(long int));
+printf("Size of a long long int: %i byte(s) ", sizof(long long int));
+printf("Size of a float: %i byte(s) ", sizof(float));
+printf("Size of a long long int: %i byte(s) ", sizeof(long long int));
+printf("Size of a float: %i byte(s) ", sizeof(float));
+return (0);
 }
