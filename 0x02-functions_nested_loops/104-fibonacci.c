8fa5ca923d4e7a3aa2b7a6741ebc633368200ff9
Theresa-web <73585864+Theresa-web@users.noreply.github.com>
2022-12-13 19:26:49 +0200

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -29,7 +29,7 @@ int main(void)
 		bef2 = (bef % l);
 		aft1 = (aft / l);
 		aft2 = (aft % l);
-    
+
 		for (i = 92; i < 99; ++i)
 		{
 			printf(", %lu", aft1 + (aft2 / l));
@@ -41,4 +41,4 @@ int main(void)
 		}
 		printf("\n");
 		return (0);
-  }
+}
