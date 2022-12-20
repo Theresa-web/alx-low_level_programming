3b896bc32ca349e9c1e35abb0b26ecae81717f9a
Constance Mhlabane <tessamhlabane31@gmail.com>
2022-12-19 23:15:15 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that takes a pointer to an interger parmeter
+ * @n: integer pointer
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
