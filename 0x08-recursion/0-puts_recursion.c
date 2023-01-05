7d37bfcbc35722c53f527574ae73307e3796c1b9
Theresa-web <73585864+Theresa-web@users.noreply.github.com>
2023-01-05 20:22:02 +0200

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,11 +5,11 @@
 */
 void _puts_recursion(char *s)
 {
-      if (*s)
-      {
-            _putchar(*s);
-            _puts_recursion(s + 1);
+     if (*s)
+     {
+          _putchar(*s);
+          _puts_recursion(s + 1);
       }
       else
-            _putchar('\n');                                                                                                   
+            _putchar('\n');                                                                                             
 }
