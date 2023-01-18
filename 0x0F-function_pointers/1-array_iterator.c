15d4208dd959c0af87635ff218698a728c5579c0
Constance Mhlabane <tessamhlabane31@gmail.com>
2023-01-18 03:22:53 -0800

    TAsk 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+	#include <stdio.h>
+	/**
+	 * arry_iterator - prints each arry elem on a newl
+	 * @array: arry
+	 * @size: how many elem to print
+	 * @action: poniter to print in regular or hex
+	 * Return: void
+	 */
+	void arry_iterator
