9712497c3104320a5d1cc221cbd87f3836544a09
Theresa-web <73585864+Theresa-web@users.noreply.github.com>
2023-01-18 13:40:41 +0200

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,8 +8,8 @@
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-  int i;
-  
+	int i;
+
 	if (array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 
@@ -18,5 +18,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 		if (cmp(array[i]))
 			return (i);
 	}
-		return (-1);
+	return (-1);
 }
