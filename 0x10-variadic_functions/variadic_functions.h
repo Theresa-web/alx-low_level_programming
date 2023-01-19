fa516a564042c6302ecab36321962e0736884b31
Theresa-web <73585864+Theresa-web@users.noreply.github.com>
2023-01-19 09:27:40 +0200

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,3 +1,10 @@
-#ifndef _VRIADIC_H_
+#ifndef _VARIADIC_H_
 #define _VARIADIC_H_
-int
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
